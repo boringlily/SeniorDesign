f053cb061ccd088bab4832d5dfade808179f8ed3
Lily Moon <53998902+BoringLily@users.noreply.github.com>
2023-03-16 09:58:22 -0400

    wireless stuff

--- controller_fw/src/main.cpp
+++ controller_fw/src/main.cpp
@@ -1,9 +1,100 @@
-#include <Arduino.h>
+/*
+  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on it.
+
+  Steps:
+  1. Connect to the access point "yourAp"
+  2. Point your web browser to http://192.168.4.1/H to turn the LED on or http://192.168.4.1/L to turn it off
+     OR
+     Run raw TCP "GET /H" and "GET /L" on PuTTY terminal with 192.168.4.1 as IP address and 80 as port
+
+  Created for arduino-esp32 on 04 July, 2018
+  by Elochukwu Ifediora (fedy0)
+*/
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+
+// Set these to your desired credentials.
+const char *ssid = "Controller_Config";
+const char *password = "yourPassword";
+
+WiFiServer server(80);
+
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  // You can remove the password parameter if you want the AP to be open.
+  // a valid password must have more than 7 characters
+  if (!WiFi.softAP(ssid, password)) {
+    log_e("Soft AP creation failed.");
+    while(1);
+  }
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+
+  Serial.println("Server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");          // print a message out the serial port
+    String currentLine = "";                // Hold data from client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // If data incoming from the client,
+        char c = client.read();             // read a byte
+        Serial.print(c);                    
+        delay(10);
+
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send an acknowlege
+          if (currentLine.length() == 0) {
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println();
+
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            // Serial.print(currentLine);
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          Serial.print("\nLed on\n");
+          digitalWrite(LED_BUILTIN, HIGH);             // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          Serial.print("\nLed off\n");
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+        if (currentLine.endsWith("GET /mavlink")) {
+          Serial.print("\nLed off\n");
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+        if (currentLine.endsWith("POST /config?=")) {
+          Serial.print("\nFetching config\n");
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }

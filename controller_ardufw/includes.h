dad0af5411e54bd9304c03ec9134173d8e005890
Lily Moon <53998902+BoringLily@users.noreply.github.com>
2023-04-21 11:23:00 -0400

    updated all libraries, added running average

---
+++ controller_ardufw/includes.h
@@ -0,0 +1,38 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <Arduino.h>
+
+// Settings
+
+#define JSON_SIZE 500
+#define BUF_SIZE 25
+
+// I/O define
+
+#define xGim1 4
+#define yGim1 5
+#define xGim2 6
+#define yGim2 7
+
+#define pot1 15
+#define pot2 16
+
+#define bi_sw1 42
+#define bi_sw2 39
+
+#define tri_sw1_pole1 41
+#define tri_sw1_pole2 40
+#define tri_sw2_pole1 38
+#define tri_sw2_pole2 37
+
+#define but1 36
+#define but2 35
+
+#define configPin 14
+
+#define RXD2 2
+#define TXD2 1
+#define CFG2 0
+
+#endif
